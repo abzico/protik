627718de40399c08fdbccad4eeddfd4575d80ef9
Wasin Thonkaew <haxpor@gmail.com>
2018-01-17 09:11:37 +0800

    Fix issue of content script not loaded when navigate URL
    We needs to ask for more permission to detect URL updated (in history)
    then inject script as needed for only https://twitter.com/*. This in
    turns added background script.

---
+++ background.js
@@ -0,0 +1,24 @@
+// background script
+
+var firstTime = true;
+
+function _cs2() { if (chrome.runtime.lastError) return; chrome.tabs.executeScript(null, { file:"constants.js" }, _cs3); }
+function _cs3() { if (chrome.runtime.lastError) return; chrome.tabs.executeScript(null, { file:"storage.js" }, _cs4); }
+function _cs4() { if (chrome.runtime.lastError) return; chrome.tabs.executeScript(null, { file:"api.js" }, _cs5); }
+function _cs5() { if (chrome.runtime.lastError) return; chrome.tabs.executeScript(null, { file:"inject.js" }); }
+
+// handle when URL changed due to history updated
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  if (chrome.runtime.lastError) return;
+
+  if (details.url === "https://twitter.com/") {
+    console.log('enter with ' + firstTime);
+    if (!firstTime) {
+      chrome.tabs.executeScript(null, { file:"buy.js" }, _cs2);
+      console.log('-inject');
+    }
+    else {
+      firstTime = false;
+    }
+  }
+});
